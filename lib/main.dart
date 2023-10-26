721f2a45bec845fab1d3b52b7af3d30400575989
EduardoHoltz <carloshltzkaap@gmail.com>
2023-10-25 21:23:36 -0300

    "Creating the project"

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('App bar'),
+        ),
+      ),
+    );
+  }
+}
